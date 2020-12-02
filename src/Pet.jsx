4a1e1aaf59f197ad83e39ca0eb8d6b3a8e1ffb73
Sharon Shmorak <sharon.shmorak@gmail.com>
2020-12-02 15:21:23 +0200

    added support for client-side routing

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Pet = function Pet({ name, key, animal, breed, media, location, id }) {
+const Pet = function Pet({ id, name, animal, breed, media, location }) {
   const image = media.length ? media[0].small : "http://placecorgi.com/300/300";
   console.log(image);
   return (
